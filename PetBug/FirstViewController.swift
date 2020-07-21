512feda78f1de5440fe8e0921676cdb576139291
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-07-21 05:13:45 -0700

    Initial Commit

---
+++ PetBug/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PetBug
+//
+//  Created by Angelina Olmedo on 7/21/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PetBug/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PetBug
+//
+//  Created by Angelina Olmedo on 7/21/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
